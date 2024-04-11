6a0450b6cfc2b46a1661a77c39137c8f8e907f62
Serdar Arici <serdararici3@gmail.com>
2024-04-11 10:55:05 +0300

    project created

---
+++ app/src/main/java/com/serdararici/travelguide/View/MainFragment.kt
@@ -0,0 +1,16 @@
+package com.serdararici.travelguide.View
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.serdararici.travelguide.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
---
+++ app/src/main/java/com/serdararici/travelguide/View/MapFragment.kt
@@ -0,0 +1,19 @@
+package com.serdararici.travelguide.View
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.serdararici.travelguide.R
+
+class MapFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+}
